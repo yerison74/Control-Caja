64ad2be16158a882effa786f30fd84d6d6026aa9
yerison74 <yerison74@gmail.com>
2025-08-05 10:08:40 -0400

    version 15.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Salón Cash Control',
+  description: 'Creado por Yerison Javier',
+  generator: 'Salón Cash Control',
 }
 
 export default function RootLayout({
